0b29a11e1481ad8eea4ecc3fba5ea7adc8c0b4b7
Bridget Reed <breed@brownsteingroup.com>
2018-03-06 12:57:14 -0500

    indexed nav

--- js/scripts.js
+++ js/scripts.js
@@ -9,33 +9,8 @@ jQuery(function ($) {
 }); // JQuery end
 
 jQuery(document).ready(function($){
-	var contentSections = $('.bcr-section'),
-		navigationItems = $('#bcr-vertical-nav a'),
-		smoothItems = $('a.smooth');
-
-	//smooth scroll nav
-	updateNavigation();
-	$(window).on('scroll', function(){
-		updateNavigation();
-	});
-
-	navigationItems.on('click', function(event){
-        event.preventDefault();
-        smoothScroll($(this.hash));
-	});
+	var smoothItems = $('a.smooth');
 	
-	function updateNavigation() {
-		contentSections.each(function(){
-			$this = $(this);
-			var activeSection = $('#bcr-vertical-nav a[href="#'+$this.attr('id')+'"]').data('number') - 1;
-			if ( ( $this.offset().top - $(window).height()/2 < $(window).scrollTop() ) && ( $this.offset().top + $this.height() - $(window).height()/2 > $(window).scrollTop() ) ) {
-				navigationItems.eq(activeSection).addClass('is-selected');
-			}else {
-				navigationItems.eq(activeSection).removeClass('is-selected');
-			}
-		});
-	}
-
 	//smooth scroll links
 	updateSmooth();
 	$(window).on('scroll', function(){
